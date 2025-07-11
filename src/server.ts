b028df0512e595b40a8be452d2e5984e9e36b983
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-07-11 17:22:05 -0300

    chore: setup environment variables

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
     validatorCompiler
 } from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
+import { env } from './env.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -15,6 +16,6 @@ app.register(fastifyCors, {
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
     console.log(`Server is running on http://localhost:3333`)
 })
