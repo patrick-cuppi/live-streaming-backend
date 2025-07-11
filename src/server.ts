0aa59aa4008c336ee240805119484a665f390607
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-07-11 17:47:07 -0300

    chore: setup biomejs

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,23 @@
+import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
 import {
-    type ZodTypeProvider,
-    serializerCompiler,
-    validatorCompiler
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:5173'
+  origin: 'http://localhost:5173',
 })
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-    return 'OK'
+  return 'OK'
 })
 
-app.listen({ port: env.PORT }).then(() => {
-    console.log(`Server is running on http://localhost:3333`)
-})
+app.listen({ port: env.PORT })
