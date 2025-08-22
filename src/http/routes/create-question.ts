92e93ae6af8f7a61dd808ec8ee87703c969ee6ad
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-08-22 19:17:46 -0300

    feat(route): create question

---
+++ src/http/routes/create-question.ts
@@ -0,0 +1,40 @@
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { db } from '../../db/connection.ts'
+import { schema } from '../../db/schema/index.ts'
+
+export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    '/rooms/:roomId/questions',
+    {
+      schema: {
+        params: z.object({
+          roomId: z.string(),
+        }),
+        body: z.object({
+          question: z.string().min(5)
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { question } = request.body
+      const { roomId } = request.params
+
+      const result = await db
+        .insert(schema.questions)
+        .values({
+          roomId,
+          question
+        })
+        .returning()
+
+      const insertedQuestion = result[0]
+      
+      if (!insertedQuestion) {
+        throw new Error('Error creating question')
+      }
+      
+      return reply.status(201).send({ roomId: insertedQuestion.id })
+    }
+  )
+}
